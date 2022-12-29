dff42f813d9347397779cc2cce85c7d63abc8d50
Sandro Wicki <sandro.wicki@outlook.com>
2022-12-29 16:12:09 +0100

    Include backup service

---
+++ Archiver/Backup/BackupProgress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ArchivePlanner.Backup
+{
+    public class BackupProgress
+    {
+        public Guid BackupId { get; private set; }
+
+        public double Progress { get; private set; }
+
+        public BackupProgress(Guid id, double progress)
+        {
+            BackupId = id;
+            Progress = progress;
+        }
+    }
+}
---
+++ Archiver/Backup/IProgressService.cs
@@ -0,0 +1,12 @@
+﻿using ArchivePlanner.Planning.Model;
+using System;
+
+namespace ArchivePlanner.Backup
+{
+    public interface IProgressService
+    {
+        IObservable<BackupProgress?> BackupProgress { get; }
+
+        void ReportProgress(Guid id, double progress);
+    }
+}
